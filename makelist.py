9f8820e032ba12c6b94813990aa48f7d955baf8d
igorkll <frerther@gmail.com>
2024-03-30 08:18:51 +0300

    upload stub

---
+++ makelist.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+
+def recursive_file_paths(folder_path):
+    file_paths = []
+    for dirpath, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            file_paths.append(os.path.join(dirpath, filename))
+    return file_paths
+
+def recursive_file_paths_without_folder(folder_path):
+    file_paths = []
+    for dirpath, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            p = os.path.join(dirpath, filename)
+            p = p[len(folder_path) + 1:len(p)]
+            file_paths.append(p)
+    return file_paths
+
+def write_paths_to_file(file_paths, output_file):
+    with open(output_file, 'w') as f:
+        lpaths = [os.path.relpath(path).replace("\\", "/") for path in file_paths]
+        formatted_paths = [f'/{lpath}' for lpath in lpaths]
+        f.write('\n'.join(formatted_paths))
+
+if __name__ == "__main__":
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    system_directory = os.path.join(current_directory, 'cstack')
+
+    write_paths_to_file(recursive_file_paths_without_folder(system_directory), os.path.join(current_directory, 'installer/filelist.txt'))
