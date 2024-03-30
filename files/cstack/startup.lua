83fe87fbe4dc2934c457d1521a25da337f35e198
igorkll <frerther@gmail.com>
2024-03-30 10:12:42 +0300

    Update startup.lua

--- files/cstack/startup.lua
+++ files/cstack/startup.lua
@@ -1,10 +1,11 @@
 local code, err = loadfile("/cstack/cstack.lua")
-if not code then
+if code then
     local ok, err = xpcall(code, debug.traceback)
     if not ok then
         shell.run("/cstack/bsod.lua", "system error (cstack.lua)", tostring(err))
     end
 else
-    shell.run("/cstack/bsod.lua", "syntax error (cstack.lua)", tostring(err))
-    os.reboot()
+    shell.run("/cstack/bsod.lua", "\"syntax error (cstack.lua)\"", "\"" .. tostring(err) .. "\"")
 end
+shell.run("/cstack/bsod.lua", "\"unexpected system shutdown\" \"\"")
+os.reboot()
