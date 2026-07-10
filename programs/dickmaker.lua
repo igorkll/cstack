221382ae8fe42e4c52519005dc14cab66ebb891e
igorkll <frerther@gmail.com>
2026-07-10 18:01:29 +0300

    Update dickmaker.lua

--- programs/dickmaker.lua
+++ programs/dickmaker.lua
@@ -1,48 +1,14 @@
 local inventorySize = 16
 
-local function move(dir)
-    if dir == -1 then
-        return turtle.down()
-    elseif dir == 0 then
-        return turtle.forward()
-    elseif dir == 1 then
-        return turtle.up()
-    end
-    return false
-end
-
-local function smartMove(dir)
-    local currentPos = {0, 0, 0}
-    local targetPos = {0, 0, 0}
-    local currentTurn = 0
-
-    local function turn(left)
-        if left then
-            turtle.turnLeft()
-            currentTurn = currentTurn + 1
-        else
-            turtle.turnRight()
-            currentTurn = currentTurn - 1
-        end
-        currentTurn = currentTurn % 4
-    end
-
-    while true do
-        if move(dir) then
-            break
-        end
-
-        turtle.turnLeft()
-    end
-end
-
 local function refuel()
     print("REFUEL")
     print("fuel level: ", turtle.getFuelLevel())
     print("fuel limit: ", turtle.getFuelLimit())
     for i = 1, inventorySize do
-        turtle.select(i)
-        turtle.refuel()
+        if turtle.getItemDetail(i) then
+            turtle.select(i)
+            turtle.refuel()
+        end
     end
 end
 
@@ -55,7 +21,23 @@ local function autoRefuel()
     end
 end
 
+local function justMove()
+    while turtle.down() do
+    end
+
+    while true do
+        if turtle.forward() then
+            break
+        else
+            turtle.turnLeft()
+        end
+    end
+end
+
 while true do
     autoRefuel()
+
+    justMove()
+
     os.sleep(0.1)
 end
