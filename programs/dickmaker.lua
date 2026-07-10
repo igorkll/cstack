a3b96349d55c280502ed28b35c991b50267d858e
igorkll <frerther@gmail.com>
2026-07-10 18:13:28 +0300

    Update dickmaker.lua

--- programs/dickmaker.lua
+++ programs/dickmaker.lua
@@ -21,23 +21,49 @@ local function autoRefuel()
     end
 end
 
-local function justMove()
-    while turtle.down() do
-    end
-
+local function justMove(moveLimit, action)
     while true do
         if turtle.forward() then
-            break
+            return true
         else
-            turtle.turnLeft()
+            action()
         end
+
+        moveLimit = moveLimit - 1
+        if moveLimit <= 0 then
+            break
+        end
+    end
+
+    return false
+end
+
+local function downMax()
+    while turtle.down() do
+    end
+end
+
+local function tryJustMove()
+    local moveLimit = 20
+    local actions
+
+    local function getRandomAction(fromOne)
+        return actions[math.random(fromOne and 1 or 2, #actions)]
+    end
+
+    actions = [getRandomAction, turtle.turnLeft, turtle.turnRight, turtle.up, turtle.down, turtle.back, downMax]
+
+    downMax()
+
+    for i = 1, 3 do
+        justMove(moveLimit, getRandomAction(true))
     end
 end
 
 while true do
     autoRefuel()
 
-    justMove()
+    tryJustMove()
 
     os.sleep(0.1)
 end
