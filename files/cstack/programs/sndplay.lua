c50c87dfb60ffd858c5e7ccd44a95b12859f817f
igorkll <frerther@gmail.com>
2026-07-20 21:31:41 +0300

    eqw

--- files/cstack/programs/sndplay.lua
+++ files/cstack/programs/sndplay.lua
@@ -1,15 +1,25 @@
-local speaker = peripheral.find("speaker")
+local args = { ... }
+
+local speaker
+local urlOrPath
+if #args == 2 then
+    speaker = peripheral.wrap(args[1])
+    urlOrPath = args[2]
+elseif #args == 1 then
+    speaker = peripheral.find("speaker")
+    urlOrPath = args[1]
+else
+    print("sndplay [speaker] <url>")
+end
+
 if not speaker then
     print("Speaker not found!")
     return
 end
 
-local args = { ... }
-local urlOrPath = args[1]
-
 local streamOrError
-if text.startwith(string, urlOrPath, "http:") or 
-    text.startwith(string, urlOrPath, "https:") then
+if text.startwith(string, urlOrPath, "http://") or 
+    text.startwith(string, urlOrPath, "https://") then
     print("Loading stream from url (" .. urlOrPath .. ")...")
     streamOrError = sndplay.loadStreamFromUrl(urlOrPath)
 else
